a1e389ae88384b0dc66323ab2423c11114f60fd9
Hynek Kydlicek <kydlicek.hynek@gmail.com>
2021-08-04 18:39:13 +0200

    Updated docker files as per PR

--- Dockerfile
+++ Dockerfile
@@ -10,18 +10,21 @@ ENV PYTHONUNBUFFERED 1
 
 
 # Install system dependencies
-RUN apt-get update && apt-get install -y netcat
+# libpq-dev needed for pyscopg-2
+RUN apt-get update && \ 
+    apt-get install -y netcat libpq-dev gcc
+
 
 RUN apt-get update && \
-    apt-get upgrade -y && 
+    apt-get upgrade -y
 
 # Install Python dependencies
 RUN pip install --upgrade pip
-COPY ./requirements.txt /usr/src/app/requirements.txt
+COPY ./requirements.txt .
 RUN pip install -r requirements.txt
 
 # Copy the current directory contents into the container at /usr/src/app
-COPY . /usr/src/app
+COPY . .
 
 # Run run_server.sh
-ENTRYPOINT ["/usr/src/app/run_server.sh"]
+ENTRYPOINT ["./run_server.sh"]
--- run_server.sh
+++ run_server.sh
@@ -1,6 +1,34 @@
 #!/bin/bash
 
-python3 ./matfyz_forum/manage.py makemigrations
-python3 ./matfyz_forum/manage.py migrate
+set -eo pipefail
+MANAGE="./matfyz_forum/manage.py"
 
-python3 ./matfyz_forum/manage.py runserver 0.0.0.0:8000
+run_server() {
+    python3 "$MANAGE" makemigrations
+    python3 "$MANAGE" migrate
+
+    # Docker binds container ip to random ip address in subnet, thefore we have to
+    # listen on all ips or set static ip at docker compose
+    python3 "$MANAGE" runserver 0.0.0.0:8000
+}
+
+if [[ -z "$1" ]]; then
+    run_server
+    exit 0
+fi
+
+case "$1" in
+    run)
+        run_server 
+        exit 0
+        ;;
+
+    manage)
+        shift
+        python3 "$MANAGE" "$@" 
+        exit 0
+        ;;
+    *)
+        echo "Wrong commad" >&2
+        exit 1
+esac
